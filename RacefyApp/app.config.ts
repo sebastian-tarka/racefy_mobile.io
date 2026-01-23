2b859142b9f2e37e4841dc79c3951da743337e6d
sebastian <twin86@gmail.com>
2026-01-23 19:55:09 +0100

    config

--- RacefyApp/app.config.ts
+++ RacefyApp/app.config.ts
@@ -56,7 +56,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     },
     edgeToEdgeEnabled: true,
     package: 'com.racefy.app',
-    versionCode: 1,
+    versionCode: 2,
     permissions: [
       'ACCESS_FINE_LOCATION',
       'ACCESS_COARSE_LOCATION',
